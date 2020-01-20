47060758ce8434917d671237c503719e8d3cc221
Anton Gepting <anton.gepting@gmail.com>
2020-01-20 19:25:34 +0100

    tests(version): add use directive

--- tests/version_tests.rs
+++ tests/version_tests.rs
@@ -1,5 +1,7 @@
 #[test]
 fn version() {
+    use tmux_interface::TmuxInterface;
+
     let mut tmux = TmuxInterface::new();
     let version = tmux.version().unwrap();
     assert_eq!(version.prog_name, "tmux");
