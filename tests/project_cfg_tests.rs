d8f1c94a442ab618aa42a263fd956c6d027aead2
Anton Gepting <anton.gepting@gmail.com>
2020-02-15 16:53:11 +0100

    tests: fix project_cfg_tests

--- tests/project_cfg_tests.rs
+++ tests/project_cfg_tests.rs
@@ -18,38 +18,41 @@ fn project_from_str() {
         sessions:
             - project_from_str:
                 detached: true
-        windows:
-            - window1:
-            - window2:
-            - window3:
-        "#;
+                windows:
+                    - window1:
+                        detached: true
+                    - window2:
+                        detached: true
+                    - window3:
+                        detached: true
+    "#;
     let project_cfg: ProjectCfg = serde_yaml::from_str(project_str).unwrap();
     assert!(project_cfg.create().is_ok());
     project_cfg.kill().unwrap();
 }
 
-//#[test]
-//fn project_get() {
-//use tmux_interface::TargetSession;
-//use tmux_project_cfg::project_cfg::ProjectCfg;
-//use tmux_project_cfg::sessions_cfg::SessionsCfg;
-//use tmux_project_cfg::{PANE_ALL, SESSION_ALL, WINDOW_ALL};
+#[test]
+fn project_get() {
+    use tmux_interface::TargetSession;
+    use tmux_project_cfg::project_cfg::ProjectCfg;
+    use tmux_project_cfg::{PANE_ALL, SESSION_ALL, WINDOW_ALL};
 
-//let project_str = r#"
-//sessions:
-//- project_get:
-//detached: true
-//windows:
-//- window1:
-//- window2:
-//- window3:
-//"#;
+    let project_str = r#"
+        sessions:
+            - project_get1:
+                detached: true
+            - project_get2:
+                detached: true
+    "#;
 
-//let project_cfg: ProjectCfg = serde_yaml::from_str(project_str).unwrap();
-//let target_session = TargetSession::new("project_get");
-//let target_sessions = vec![&target_session];
+    let project_cfg: ProjectCfg = serde_yaml::from_str(project_str).unwrap();
+    assert!(project_cfg.create().is_ok());
 
-////let project_cfg = ProjectCfg::get();
-//assert!(SessionsCfg::get(&target_sessions, SESSION_ALL, WINDOW_ALL, PANE_ALL).is_ok());
-//project_cfg.kill().unwrap();
-//}
+    let target_session1 = TargetSession::Raw("project_get1");
+    let target_session2 = TargetSession::Raw("project_get2");
+    let target_sessions = vec![&target_session1, &target_session2];
+
+    assert!(ProjectCfg::get(&target_sessions, SESSION_ALL, WINDOW_ALL, PANE_ALL).is_ok());
+
+    project_cfg.kill().unwrap();
+}
